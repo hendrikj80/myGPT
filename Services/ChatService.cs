5bab8e84edef7361ecee290dc4116ba3787732c0
hendrikj80 <hendrik@startup1.de>
2023-04-12 20:41:42 +0200

    Add files via upload

---
+++ Services/ChatService.cs
@@ -0,0 +1,184 @@
+﻿using Cosmos.Chat.GPT.Models;
+
+namespace Cosmos.Chat.GPT.Services;
+
+public class ChatService
+{
+
+    //All data is cached in chatSessions List object.
+    private static List<ChatSession> chatSessions = new();
+
+    private readonly CosmosService _cosmosDbService;
+    private readonly OpenAiService _openAiService;
+    private readonly int maxConversationLength;
+
+
+    public ChatService(CosmosService cosmosService, OpenAiService openAiService)
+    {
+        _cosmosDbService = cosmosService;
+        _openAiService = openAiService;
+
+        maxConversationLength = openAiService.MaxTokens / 2;
+    }
+
+    // Returns list of chat session ids and names for left-hand nav to bind to (display ChatSessionName and ChatSessionId as hidden)
+    public async Task<List<ChatSession>> GetAllChatSessionsAsync()
+    {
+        chatSessions = await _cosmosDbService.GetChatSessionsAsync();
+
+        return chatSessions;
+    }
+
+    //Returns the chat messages to display on the main web page when the user selects a chat from the left-hand nav
+    public async Task<List<ChatMessage>> GetChatSessionMessagesAsync(string? chatSessionId)
+    {
+        ArgumentNullException.ThrowIfNull(chatSessionId);
+
+        List<ChatMessage> chatMessages = new();
+
+        if (chatSessions.Count == 0)
+        {
+            return Enumerable.Empty<ChatMessage>().ToList();
+        }
+
+        int index = chatSessions.FindIndex(s => s.ChatSessionId == chatSessionId);
+
+        if (chatSessions[index].Messages.Count == 0)
+        {
+            //Messages are not cached, go read from database
+            chatMessages = await _cosmosDbService.GetChatSessionMessagesAsync(chatSessionId);
+
+            //cache results
+            chatSessions[index].Messages = chatMessages;
+
+        }
+        else
+        {
+            //load from cache
+            chatMessages = chatSessions[index].Messages;
+        }
+        return chatMessages;
+
+    }
+
+    //User creates a new Chat Session
+    public async Task CreateNewChatSessionAsync()
+    {
+        ChatSession chatSession = new();
+
+        chatSessions.Add(chatSession);
+
+        await _cosmosDbService.InsertChatSessionAsync(chatSession);
+
+    }
+
+    //User Inputs a chat from "New Chat" to user defined
+    public async Task RenameChatSessionAsync(string? chatSessionId, string newChatSessionName)
+    {
+        ArgumentNullException.ThrowIfNull(chatSessionId);
+
+        int index = chatSessions.FindIndex(s => s.ChatSessionId == chatSessionId);
+
+        chatSessions[index].ChatSessionName = newChatSessionName;
+
+        await _cosmosDbService.UpdateChatSessionAsync(chatSessions[index]);
+
+    }
+
+    //User deletes a chat session
+    public async Task DeleteChatSessionAsync(string? chatSessionId)
+    {
+        ArgumentNullException.ThrowIfNull(chatSessionId);
+
+        int index = chatSessions.FindIndex(s => s.ChatSessionId == chatSessionId);
+
+        chatSessions.RemoveAt(index);
+
+        await _cosmosDbService.DeleteChatSessionAsync(chatSessionId);
+
+
+    }
+
+    //User prompt to ask _openAiService a question
+    public async Task<string> AskOpenAiAsync(string? chatSessionId, string prompt)
+    {
+        ArgumentNullException.ThrowIfNull(chatSessionId);
+
+        await AddPromptMessageAsync(chatSessionId, prompt);
+
+        string conversation = GetChatSessionConversation(chatSessionId);
+
+        string response = await _openAiService.AskAsync(chatSessionId, conversation);
+
+        await AddResponseMessageAsync(chatSessionId, response);
+
+        return response;
+
+    }
+
+    private string GetChatSessionConversation(string chatSessionId)
+    {
+        string conversation = "";
+
+        int index = chatSessions.FindIndex(s => s.ChatSessionId == chatSessionId);
+
+        if (chatSessions[index].Messages.Count > 0)
+        {
+            List<ChatMessage> chatMessages = chatSessions[index].Messages;
+
+
+            foreach (ChatMessage chatMessage in chatMessages)
+            {
+
+                conversation += chatMessage.Text + "\n";
+
+            }
+
+            if (conversation.Length > maxConversationLength)
+                conversation = conversation.Substring(conversation.Length - maxConversationLength, maxConversationLength);
+
+        }
+
+        return conversation;
+    }
+
+    public async Task<string> SummarizeChatSessionNameAsync(string? chatSessionId, string prompt)
+    {
+        ArgumentNullException.ThrowIfNull(chatSessionId);
+
+        prompt += "\n\n Summarize this prompt in one or two words to use as a label in a button on a web page";
+        string response = await _openAiService.AskAsync(chatSessionId, prompt);
+
+        await RenameChatSessionAsync(chatSessionId, response);
+
+        return response;
+
+    }
+
+    // Add human prompt to the chat session message list object and insert into _cosmosDbService.
+    private async Task AddPromptMessageAsync(string chatSessionId, string text)
+    {
+        ChatMessage chatMessage = new(chatSessionId, "Human", text);
+
+        int index = chatSessions.FindIndex(s => s.ChatSessionId == chatSessionId);
+
+        chatSessions[index].AddMessage(chatMessage);
+
+        await _cosmosDbService.InsertChatMessageAsync(chatMessage);
+
+    }
+
+    // Add _openAiService response to the chat session message list object and insert into _cosmosDbService.
+    private async Task AddResponseMessageAsync(string chatSessionId, string text)
+    {
+        ChatMessage chatMessage = new(chatSessionId, "AI", text);
+
+        int index = chatSessions.FindIndex(s => s.ChatSessionId == chatSessionId);
+
+        chatSessions[index].AddMessage(chatMessage);
+
+        await _cosmosDbService.InsertChatMessageAsync(chatMessage);
+
+    }
+
+}
---
+++ Services/OpenAiService.cs
@@ -0,0 +1,55 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+
+namespace Cosmos.Chat.GPT.Services;
+
+public class OpenAiService
+{
+    private readonly string _endpoint;
+    private readonly string _key;
+    private readonly string _deploymentName;
+    private readonly int _maxTokens;
+    private readonly OpenAIClient _client;
+
+    public int MaxTokens
+    {
+        get => _maxTokens;
+    }
+
+    public OpenAiService(string endpoint, string key, string deploymentName, string maxTokens)
+    {
+        ArgumentNullException.ThrowIfNull(endpoint);
+        ArgumentNullException.ThrowIfNull(key);
+        ArgumentNullException.ThrowIfNull(deploymentName);
+        ArgumentNullException.ThrowIfNull(maxTokens);
+
+        _endpoint = endpoint;
+        _key = key;
+        _deploymentName = deploymentName;
+        _maxTokens = Int32.TryParse(maxTokens, out _maxTokens) ? _maxTokens : 3000;
+
+        OpenAIClient client = new(new Uri(_endpoint), new AzureKeyCredential(_key));
+
+        _client = client ??
+            throw new ArgumentException("Unable to connect to existing Azure OpenAI endpoint.");
+    }
+
+    public async Task<string> AskAsync(string chatSessionId, string prompt)
+    {
+        CompletionsOptions completionsOptions = new()
+        {
+            Prompt = { prompt },
+            User = chatSessionId,
+            MaxTokens = _maxTokens
+
+            //Temperature = 1,
+            //Model = "text-davinci-003",
+            //FrequencyPenalty = 0,
+            //PresencePenalty = 0
+        };
+
+        Response<Completions> completionsResponse = await _client.GetCompletionsAsync(_deploymentName, completionsOptions);
+
+        return completionsResponse.Value.Choices[0].Text;
+    }
+}
