b0f50b71b51ce29d621d9cadc72c51d767f544d6
hendrikj80 <hendrik@startup1.de>
2023-04-12 16:50:23 +0200

    v0.2

--- services/chatservice.cs
+++ services/chatservice.cs
@@ -1,1 +1,18 @@
-//chat interface using openai api to send and receivce message 
+//If chatButton is clicked get value of chatInput and send it to the server
+chatButton.onClick.AddListener(() => {
+    if (chatInput.text != "") {
+        //disable chatInput
+        chatInput.interactable = false;
+        //Send message to server
+        socket.Emit("chat message", chatInput.text);
+        //place message in Div ChatBox 
+        chatBox.text += "You: " + chatInput.text + "
+        //Clear chatInput   
+        chatInput.text = "";
+        //enabel chatInput
+        chatInput.interactable = true;
+    }
+});
+
+
+
